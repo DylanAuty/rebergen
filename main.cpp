e549fc9a8dd7e609dfc1ee2573adba2c86853c0b
Dylan Auty <dylan.auty@gmail.com>
2016-01-27 12:53:12 +0000

    Seeding rand() at top of main because of reasons

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 int main(){
+	srand(time(NULL));
 	
 	reberNode* currentNode = NULL;
 	reberNode n1(2), n2(2), n3(2);
 	vector<reberNode*> v1 = {&n1, &n2};
 	vector<char> c1 = {'B', 'C'};
 	n1.connectToNodes(v1, c1);
-	srand(time(NULL));
 	n1.transition();
 	return 0;
 }
