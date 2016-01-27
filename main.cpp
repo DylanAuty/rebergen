30ec9af0630387100984282b3f6b5b16babee9b3
Dylan Auty <dylan.auty@gmail.com>
2016-01-27 13:06:05 +0000

    Added argument handling for one string at a time

--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
+/** main.cpp
+ * Main function used by the Reber Grammar string generator
+ * Dylan Auty, January 2016
+ */
 
 #include <iostream>
-#include "reberNode.hpp"
 #include <ctime>
+#include <cstdlib>
+#include "reberNode.hpp"
 
 using namespace std;
 
-int main(){
-	srand(time(NULL));
-	
+int main(int argc, char* argv[]){
+	srand(time(NULL));	// Seed rand()
+	if (argc != 2){
+		cerr << "ERROR: Please enter only one argument." << endl;
+		exit(1);
+	}
+	else {
+		switch(argv[1]):
+		case "-h":
+			printHelp();
+			return 0;
+		case "-r":
+			printReberString();
+			return 0;
+		case "-e":
+			printEmbeddedReberString();
+			return 0;
+	}
+
 	reberNode* currentNode = NULL;
 	reberNode n1(2), n2(2), n3(2);
 	vector<reberNode*> v1 = {&n1, &n2};
